2fbb96bf84703f33a3dad175d406cbb1a340cf70
Moghany <m.osama.elmoghany@gmail.com>
2024-03-04 13:08:05 +0200

    more class based views. added reviews page & each review details page

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     # path("", views.review),
+    # path("thank-you", views.thank_you)
+    path("thank-you", views.ThankYouView.as_view()),
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you)
+    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/<int:id>", views.SingleReviewView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -22,36 +24,62 @@ class ReviewView(View):
             "form": form
         })  
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+    #to pass variables => use get_context_data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'this works'
+        return context
+    # def get(self, request):
+    #     return render(request, "reviews/thank_you.html")
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs['id']
+        selected_review = Review.objects.get(pk=review_id)
+        context['review'] = selected_review
+        return context
+
+# def review(request):
+#     if request.method == "POST":
+#         #for updating a specific data entry
+#         # existing_model = Review.objects.get(pk=1)
+#         # form = ReviewForm(request.POST, instance=existing_model)
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             # review = Review(user_name=form.cleaned_data['user_name'],
+#             #                 review_text=form.cleaned_data['review_text'],
+#             #                 rating=form.cleaned_data['rating']
+#             #                 )
+#             # review.save()
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+#     #     entered_username = request.POST['user']
+#     #     if entered_username == "" and len(entered_username) >= 100:
+#     #         return render(request, "reviews/review.html", {
+#     #             "has_error": True
+#     #         })
+#     #     print("username: ", entered_username)
+#     else: 
+#         form = ReviewForm()
     
-def review(request):
-    if request.method == "POST":
-        #for updating a specific data entry
-        # existing_model = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=existing_model)
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            # review = Review(user_name=form.cleaned_data['user_name'],
-            #                 review_text=form.cleaned_data['review_text'],
-            #                 rating=form.cleaned_data['rating']
-            #                 )
-            # review.save()
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-    #     entered_username = request.POST['user']
-    #     if entered_username == "" and len(entered_username) >= 100:
-    #         return render(request, "reviews/review.html", {
-    #             "has_error": True
-    #         })
-    #     print("username: ", entered_username)
-    else: 
-        form = ReviewForm()
-    
-    return render(request, "reviews/review.html", {
-        # "has_error": False
-        "form": form
-    })    
+#     return render(request, "reviews/review.html", {
+#         # "has_error": False
+#         "form": form
+#     })    
 
     
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
