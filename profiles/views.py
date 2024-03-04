2fd3d1809f8553b733cbd56e0f97f2ed64bbe4da
Moghany <m.osama.elmoghany@gmail.com>
2024-03-04 23:34:49 +0200

    adding a model for the image

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+
+from .forms import ProfileForm
+from .models import UserProfile
 # Create your views here.
 
-
+    
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
     def post(self, request):
-        img_file = request.FILES["image"]
-        print(img_file)
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILE['user_image'])
+            profile.save()
+            # img_file = request.FILES["image"]
+            # store_file(img_file)
+            return HttpResponseRedirect("/profiles")
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
